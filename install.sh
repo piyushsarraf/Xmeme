d2666b9634c546f95ae0cc48e6cb1b057df5a13a
root <root@KenAltruse.localdomain>
2021-02-14 16:57:08 +0530

    Check

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+# Any installation related commands
+
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+sudo apt update
+sudo apt install -y nodejs npm mongodb-org
+sudo service mongod start 
+
+
+# Any configuration related commands
---
+++ server_run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+NODE_ENV=production node app.js
+
+
+# Setup DB or any other environment variables you want to setup.
+
+
+npm install 
+
+node app.js
